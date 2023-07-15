315bf2f8832c213d307009b09cd5fbfa61d546cb
RahmaaEsmail <rahmaaesmail26@gmail.com>
2023-07-15 12:53:26 +0300

    Add data to iframe

--- js/app.js
+++ js/app.js
@@ -1,28 +1,30 @@
-document.getElementById("htmlCode").value="<div>\n\n</div>";
-document.getElementById("cssCode").value="<style>\n\n</style>";
-document.getElementById("jsCode").value="<script>\n\n</script>";
+document.getElementById("htmlCode").value = "<div>\n\n</div>";
+document.getElementById("cssCode").value = "<style>\n\n</style>";
+document.getElementById("jsCode").value = "<script>\n\n</script>";
 let list = [];
 
+
 const init = () => {
-    const data = getData()
+    const data = getDataFromStorage()
     displayData(data);
 }
 init()
 
-function showPreview(){
-    var htmlCode = document.getElementById("htmlCode").value;
-    var cssCode = ""+document.getElementById("cssCode").value+"";
-    var jsCode = ""+document.getElementById("jsCode").value+"";
+function showPreview() {
     
+    var htmlCode = document.getElementById("htmlCode").value;
+    var cssCode = "" + document.getElementById("cssCode").value + "";
+    var jsCode = "" + document.getElementById("jsCode").value + "";
+
     var frame = document.getElementById("preview-window").contentWindow.document;
     frame.open();
-    frame.write(htmlCode+cssCode+jsCode);
+    frame.write(htmlCode + cssCode + jsCode);
     frame.close();
 }
 
 document.querySelectorAll("textarea").forEach(ele => {
-    ele.addEventListener("change",()=>{
-       addData(ele.id)
+    ele.addEventListener("change", () => {
+        addData(ele.id)
     })
 })
 
@@ -32,56 +34,57 @@ const addData = (item) => {
         [item]: ele || ''
     }
     list.push(obj)
-    storeData(list)
+    storeDataToStorage(list)
 }
 
-const storeData = (list) => {
+const storeDataToStorage = (list) => {
     localStorage.setItem("Data", JSON.stringify(list))
 }
 
-function getData (){
-    if(localStorage.getItem("Data")) {
-     list = JSON.parse(localStorage.getItem("Data"))
-     return list
+function getDataFromStorage() {
+    if (localStorage.getItem("Data")) {
+        list = JSON.parse(localStorage.getItem("Data"))
+        return list
     }
     return []
 }
 
-function displayData  (data) {
-   data.forEach(ele => {
-       var frame = document.getElementById("preview-window").contentWindow.document;
-       if(ele['htmlCode']) 
-       frame.write(ele["htmlCode"]);
-       if(ele["cssCode"]) 
-       frame.write(ele['cssCode']);
-       if(ele['jsCode'])
-       frame.write(ele['jsCode'])
+function displayData(data) {
+    data.forEach(ele => {
+        if (ele) {
+            Object.keys(ele).forEach(item => {
+                document.querySelector(`#${item}`).value = ele[item]
+                showPreview()
+            })
+        }
     })
 }
 
 
 
-function show(x){
-    document.getElementById("html").style.display="none";
-    document.getElementById("css").style.display="none";
-    document.getElementById("js").style.display="none";
-    document.getElementById("result").style.display="none";
-    document.getElementById(x).style.display="block";
+function show(x) {
+    document.getElementById("html").style.display = "none";
+    document.getElementById("css").style.display = "none";
+    document.getElementById("js").style.display = "none";
+    document.getElementById("result").style.display = "none";
+    document.getElementById(x).style.display = "block";
 }
 
-function show_all(){
-    if(window.innerWidth>=992)
-    {
-        document.getElementById("html").style.display="block";
-        document.getElementById("css").style.display="block";
-        document.getElementById("js").style.display="block";
-        document.getElementById("result").style.display="block";
+function show_all() {
+    if (window.innerWidth >= 992) {
+        document.getElementById("html").style.display = "block";
+        document.getElementById("css").style.display = "block";
+        document.getElementById("js").style.display = "block";
+        document.getElementById("result").style.display = "block";
     }
-    if(window.innerWidth<992 && document.getElementById("html").style.display=="block")
-    {
-        document.getElementById("css").style.display="none";
-        document.getElementById("js").style.display="none";
-        document.getElementById("result").style.display="none";
+    if (window.innerWidth < 992 && document.getElementById("html").style.display == "block") {
+        document.getElementById("css").style.display = "none";
+        document.getElementById("js").style.display = "none";
+        document.getElementById("result").style.display = "none";
     }
 }
 
+
+document.querySelectorAll("textarea").forEach(ele => {
+    ele.addEventListener("input", showPreview)
+})
